f46ca7c94d7a19983d32aa8a443511da470d4a34
phu_huynh <phuhh98@gmail.com>
2024-11-06 12:08:17 +0700

    astro for react tailwind and spotlight dev tool

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 // @ts-check
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   base: "/blog",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react(), tailwind(), sentry(), spotlightjs()],
   site: "https://phuhh98.github.io",
 });
