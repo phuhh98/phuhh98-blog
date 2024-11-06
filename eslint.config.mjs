89099566e1f514afa90a06a14104bf1ab3f43e89
phu_huynh <phuhh98@gmail.com>
2024-11-06 11:11:04 +0700

    setup prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
-import { includeIgnoreFile } from '@eslint/compat';
-import eslint from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginAstro from 'eslint-plugin-astro';
-import markdown from 'eslint-plugin-markdown';
-import * as mdx from 'eslint-plugin-mdx';
-import perfectionist from 'eslint-plugin-perfectionist';
-import reactPlugin from 'eslint-plugin-react';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import tseslint from 'typescript-eslint';
+import { includeIgnoreFile } from "@eslint/compat";
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginAstro from "eslint-plugin-astro";
+import markdown from "eslint-plugin-markdown";
+import * as mdx from "eslint-plugin-mdx";
+import perfectionist from "eslint-plugin-perfectionist";
+import reactPlugin from "eslint-plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, '.gitignore');
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default tseslint.config(
   // Ref: https://typescript-eslint.io/getting-started/#step-2-configuration
@@ -21,9 +21,9 @@ export default tseslint.config(
   // Ref: https://ota-meshi.github.io/eslint-plugin-astro/user-guide/#-usage
   ...eslintPluginAstro.configs.recommended,
   // Ref https://perfectionist.dev/configs/recommended-natural
-  perfectionist.configs['recommended-natural'],
+  perfectionist.configs["recommended-natural"],
   ...markdown.configs.recommended,
-  reactPlugin.configs.flat['jsx-runtime'],
+  reactPlugin.configs.flat["jsx-runtime"],
   {
     ...mdx.flat,
     // optional, if you want to lint code blocks at the same
@@ -35,21 +35,21 @@ export default tseslint.config(
     }),
   },
   {
-    files: ['**/*.md', '**/*.mdx'],
+    files: ["**/*.md", "**/*.mdx"],
     rules: {
-      'no-irregular-whitespace': ['off'],
+      "no-irregular-whitespace": ["off"],
     },
   },
   {
-    files: ['**/*.mdx'],
+    files: ["**/*.mdx"],
     rules: {
-      '@typescript-eslint/no-unused-vars': ['off'],
+      "@typescript-eslint/no-unused-vars": ["off"],
     },
   },
   {
     rules: {
-      '@typescript-eslint/triple-slash-reference': ['off'],
-      'no-console': ['error'],
+      "@typescript-eslint/triple-slash-reference": ["off"],
+      "no-console": ["error"],
     },
   },
   includeIgnoreFile(gitignorePath),
