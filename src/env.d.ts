40b18fe39cef3e219f5c177a993889dc83f07259
Phu Huynh <phu_huynh@epam.com>
2025-09-22 17:09:43 +0700

    audit packages

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,7 +5,7 @@ interface ImportMeta {
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
-  readonly PUBLIC_APP_BASE_URL: sttring;
+  readonly PUBLIC_APP_BASE_URL: string;
   readonly PUBLIC_AUTHOR_EMAIL: string;
   readonly PUBLIC_GITHUB_PROFILE_URL: string;
   readonly PUBLIC_LINKEDIN_PROFILE_URL: string;
@@ -20,12 +20,3 @@ interface Window {
 // Google analytic gtag
 // eslint-disable-next-line
 declare var dataLayer: any[];
-
-declare module "decap-cms-media-library-cloudinary" {
-  import type { CmsMediaLibrary } from "decap-cms-core";
-  export const DecapCmsMediaLibraryCloudinary: CmsMediaLibrary;
-
-  const cloudinaryMediaLibrary: CmsMediaLibrary;
-
-  export default cloudinaryMediaLibrary;
-}
